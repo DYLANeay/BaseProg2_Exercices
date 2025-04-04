d58180db233876d0d38a05cf9385cacf6c0d810c
dylan <185331015+DYLANeay@users.noreply.github.com>
2025-04-04 18:00:54 +0200

    finishing exercice

--- src/serie4/ex3/Main.java
+++ src/serie4/ex3/Main.java
@@ -2,6 +2,17 @@ package serie4.ex3;
 
 public class Main {
     public static void main(String[] args) {
+        SmartDevice p1 = new SmartPhone(90);
+        SmartDevice w1 = new SmartWatch(0);
+
+        ((SmartWatch) w1).addSteps(200);
+        ((SmartWatch) w1).addSteps(200);
+
+        p1.turnOn();
+        w1.turnOn();
+
+        p1.performFunction();
+        w1.performFunction();
 
     }
 }
